9792d91aba713a0157a8ec4d815bad6743c38773
hee lee <leehee1on@gmail.com>
2019-09-26 21:11:23 +0900

    ip

--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import './Movie.css';
 
-function Movie({id, year, title, summary,poster}){
-    return <h1>{title}</h1> 
+
+function Movie({id, year, title, summary,poster,genres}){
+    return <div className="movie">
+            <img src={poster} alt={title} title={title}/>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                     {genres.map((genre,index) => 
+                    <li key={index} className="genres__genre">{genre}</li> )}</ul>
+                <p className="movie__summary">{summary.slice(0,140)}...</p>
+            </div>
+    </div>
 };
-/////asdsaddad
+/////
 // propTypes 라고 쓰길 권장 자주하는 실수
 Movie.propTypes = {
  id: PropTypes.number.isRequired,
  year: PropTypes.number.isRequired,
  title: PropTypes.string.isRequired,
  summary: PropTypes.string.isRequired,
- poster: PropTypes.string.isRequired
+ poster: PropTypes.string.isRequired,
+ genres:PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
