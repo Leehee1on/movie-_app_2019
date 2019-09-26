715bf7440eaddbc42c9c7e235fd48941fdd12d2a
hee lee <leehee1on@gmail.com>
2019-09-26 14:51:22 +0900

    a

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function Movie({id, year, title, summary,poster}){
     return <h1>{title}</h1> 
 };
-
+/////asdsaddad
 // propTypes 라고 쓰길 권장 자주하는 실수
 Movie.propTypes = {
  id: PropTypes.number.isRequired,
